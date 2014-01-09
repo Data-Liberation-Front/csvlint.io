4b28fac4236d909cdfbead52309d368be002fab3
James Smith <james@floppy.org.uk>
2014-01-09 13:29:31 +0000

    Add URL checking before accepting for validation

--- app/controllers/validation_controller.rb
+++ app/controllers/validation_controller.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 class ValidationController < ApplicationController
 
   def index
@@ -8,7 +10,17 @@ class ValidationController < ApplicationController
   end
 
   def validate
+    # Check we have a URL
     @url = params[:url]
+    redirect_to root_path and return if @url.nil?
+    # Check it's valid
+    @url = begin
+      URI.parse(@url)
+    rescue URI::InvalidURIError
+      redirect_to root_path and return
+    end
+    # Check scheme
+    redirect_to root_path and return unless ['http', 'https'].include?(@url.scheme)
   end
 
 end
--- spec/controllers/validation_controller_spec.rb
+++ spec/controllers/validation_controller_spec.rb
@@ -9,4 +9,37 @@ describe ValidationController do
     end
   end
 
+  describe "GET 'validate'" do
+
+    it "redirects to root if no URL is supplied" do
+      get 'validate'
+      response.should be_redirect
+      response.location.should == root_url
+    end
+
+    it "redirects to root if an invalid url is supplied" do
+      get 'validate', url: 'complete@balls:/'
+      response.should be_redirect
+      response.location.should == root_url
+    end
+
+    it "redirects to root if a relative url is supplied" do
+      get 'validate', url: '/test.csv'
+      response.should be_redirect
+      response.location.should == root_url
+    end
+
+    it "redirects to root if a non-http[s] url is supplied" do
+      get 'validate', url: 'file://test.csv'
+      response.should be_redirect
+      response.location.should == root_url
+    end
+
+    it "returns http success if a sensible url is supplied" do
+      get 'validate', url: 'http://example.com/test.csv'
+      response.should be_success
+    end
+
+  end
+
 end
