eb134616ca57a07fa199666b3ec14ef782542e99
James Smith <james@floppy.org.uk>
2014-01-09 13:54:54 +0000

    Basic bootstrap CDN deuglification

--- spec/views/validation/index.html.erb_spec.rb
+++ spec/views/validation/index.html.erb_spec.rb
@@ -4,8 +4,7 @@ describe "validation/index.html.erb" do
   
   it "should contain a URL entry box" do
     render
-    rendered.should include "Enter CSV URL"
-    rendered.should include %{<input id="url" name="url" type="url" />}
+    rendered.should include %{<input class="form-control" id="url" name="url" placeholder="Enter CSV URL" type="url" value="" />}
   end
   
 end
