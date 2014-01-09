8f63d6c1af05294c2637fa805410d9cccdb4a5ab
James Smith <james@floppy.org.uk>
2014-01-09 13:29:05 +0000

    use url_field in index page

--- spec/views/validation/index.html.erb_spec.rb
+++ spec/views/validation/index.html.erb_spec.rb
@@ -5,7 +5,7 @@ describe "validation/index.html.erb" do
   it "should contain a URL entry box" do
     render
     rendered.should include "Enter CSV URL"
-    rendered.should include %{<input id="url" name="url" type="text" />}
+    rendered.should include %{<input id="url" name="url" type="url" />}
   end
   
 end
