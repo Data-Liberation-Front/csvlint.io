42965a60c8928d56db65146c8f6fdbae71070013
James Smith <james@floppy.org.uk>
2014-01-13 11:07:23 +0000

    Merge warnings and error counts into a single line

--- app/helpers/validation_helper.rb
+++ app/helpers/validation_helper.rb
@@ -1,2 +1,17 @@
 module ValidationHelper
+
+  def error_and_warning_count(errors, warnings, options)
+    # Generate string
+    components = []
+    components << pluralize(@errors.count, t(:error).titleize) unless @errors.empty?
+    components << pluralize(@warnings.count, t(:warning).titleize) unless @warnings.empty?
+    if components.empty?
+      ''
+    else
+      # Wrap it up
+      wrapper = options[:wrapper] || :span
+      content_tag(wrapper, components.join(', '))
+    end
+  end
+
 end
