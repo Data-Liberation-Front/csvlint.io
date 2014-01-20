e87013bb2a852e9914a92c4cd4f1c01812cde417
Leigh Dodds <leigh@ldodds.com>
2014-01-20 11:48:35 +0000

    Fix up tests

--- spec/views/validation/validate.html.erb_spec.rb
+++ spec/views/validation/validate.html.erb_spec.rb
@@ -8,8 +8,8 @@ describe "validation/_message.html.erb" do
       :encoding => "Your CSV appears to be encoded in <code>iso-8859-1</code>. We recommend you use <code>UTF-8</code>.",
       :no_encoding => "The URL your CSV is referenced at appears to not be sending an encoding header with its content-type.",
       :invalid_encoding => "Your CSV appears to be encoded in <code>iso-8859-1</code>, but invalid characters were found. This can often be caused by copying and pasting data from a different source.",
-      :wrong_content_type => "The URL your CSV is referenced at appears to be sending the wrong content-type header. We recommend that you configure your server to send the <code>text/csv</code> content-type header",
-      :no_content_type => "The URL your CSV is referenced at does not appear to be sending a content-type header. We recommend that you configure your server to send the <code>text/csv</code> content-type header"
+      :wrong_content_type => "The URL your CSV is referenced at appears to be sending the wrong <code>Content-Type</code> header. We recommend that you configure your server to send the <code>text/csv</code> content-type header",
+      :no_content_type => "The URL your CSV is referenced at does not appear to be sending a <code>Content-Type</code> header. We recommend that you configure your server to send the <code>text/csv</code> content-type header"
     }.each do |k, v|
       
       message = Csvlint::ErrorMessage.new(:type => k)
