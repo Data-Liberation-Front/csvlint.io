0e6719181dcec6f8c14a59043ddef85101330b13
Leigh Dodds <leigh@ldodds.com>
2014-02-11 17:35:04 +0000

    Initial support for upload schema to drive validation

--- app/controllers/validation_controller.rb
+++ app/controllers/validation_controller.rb
@@ -8,9 +8,14 @@ class ValidationController < ApplicationController
 
   def redirect
     if !params["url"].blank? 
-      redirect_to validate_path(url: params["url"], schema: params[:schema_url])
+      redirect_to validate_path(url: params["url"], schema_url: params[:schema_url])
     elsif !params["file"].blank? 
-      validate_csv(File.new(params[:file].tempfile))
+      @schema = nil
+      if params[:schema_file]
+        schema_json = JSON.parse( File.new( params[:schema_file].tempfile ).read() )
+        @schema = Csvlint::Schema.from_json_table( nil, schema_json )
+      end
+      validate_csv(File.new(params[:file].tempfile), @schema)
       @file = File.new(params[:file].tempfile)
       respond_to do |wants|
         wants.html { render "validation/validate"  }
@@ -25,7 +30,6 @@ class ValidationController < ApplicationController
   def validate
     # Check we have a URL
     @url = params[:url]
-    @schema_url = params[:schema]
     redirect_to root_path and return if @url.nil? && @file.nil?
     # Check it's valid
     @url = begin
@@ -35,7 +39,9 @@ class ValidationController < ApplicationController
     end
     # Check scheme
     redirect_to root_path and return unless ['http', 'https'].include?(@url.scheme)
-    validate_csv(@url.to_s, @schema_url)
+    @schema_url = params[:schema_url]
+    schema = Csvlint::Schema.load_from_json_table(@schema_url) 
+    validate_csv(@url.to_s, schema)
     # Responses
     respond_to do |wants|
       wants.html
@@ -49,9 +55,8 @@ class ValidationController < ApplicationController
   
     def validate_csv(io, schema = nil)
       # Load schema if set
-      if schema
-        @schema = Csvlint::Schema.load_from_json_table(schema) 
-        if @schema.nil? || @schema.fields.empty?
+      if params[:schema_url]
+        if schema.nil? || schema.fields.empty?
           @schema_error = Csvlint::ErrorMessage.new(
             type: :invalid_schema,
             category: :schema
@@ -59,7 +64,7 @@ class ValidationController < ApplicationController
         end
       end
       # Validate
-      @validator = Csvlint::Validator.new( io, nil, @schema )
+      @validator = Csvlint::Validator.new( io, nil, schema )
       @warnings = @validator.warnings
       @errors = @validator.errors
       @errors.prepend(@schema_error) if @schema_error
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,7 +47,7 @@ RSpec.configure do |config|
 end
 
 def load_fixture(filename)
-  File.read(File.join(Rails.root, 'fixtures', filename)).gsub("\n", "\r\n")
+  File.read(File.join(Rails.root, 'fixtures', filename))
 end
 
 def mock_csv(url, file)
