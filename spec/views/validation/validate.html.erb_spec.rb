81b9f10f56c3ce8fa5c10499073ebaf83e5802b7
Leigh Dodds <leigh@ldodds.com>
2014-02-11 12:50:56 +0000

    Improve error messages with additional feedback. Provide hook for supplying extra guidance

--- spec/views/validation/validate.html.erb_spec.rb
+++ spec/views/validation/validate.html.erb_spec.rb
@@ -6,16 +6,19 @@ describe "validation/_message.html.erb" do
     
     {
       :encoding => "Your CSV appears to be encoded in <code>iso-8859-1</code>. We recommend you use <code>UTF-8</code>.",
-      :no_encoding => "The URL your CSV is referenced at appears to not be sending an encoding header with its content-type.",
-      :invalid_encoding => "Your CSV appears to be encoded in <code>iso-8859-1</code>, but invalid characters were found. This can often be caused by copying and pasting data from a different source.",
-      :wrong_content_type => "The URL your CSV is referenced at appears to be sending the wrong <code>Content-Type</code> header. We recommend that you configure your server to send the <code>text/csv</code> content-type header",
-      :no_content_type => "The URL your CSV is referenced at does not appear to be sending a <code>Content-Type</code> header. We recommend that you configure your server to send the <code>text/csv</code> content-type header"
+      :no_encoding => "The encoding of your CSV file is not being declared in the HTTP response.",
+      :invalid_encoding => "Your CSV appears to be encoded in <code>iso-8859-1</code>, but invalid characters were found",
+      :wrong_content_type => "Your CSV file is being delivered with an incorrect <code>Content-Type</code>",
+      :no_content_type => "Your CSV file is being delivered without a <code>Content-Type</code> header"
     }.each do |k, v|
       
       message = Csvlint::ErrorMessage.new(:type => k)
       validator = double("validator")
       validator.stub(:encoding) { "iso-8859-1" }
-      
+      validator.stub(:content_type) { "text/plain" }
+      validator.stub(:extension) { ".csv" }
+      validator.stub(:headers) { {"content-type" => "text/plain"} }
+        
       validator 
       render :partial => "validation/message", :locals => { :message => message, :validator => validator }
       
