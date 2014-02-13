75945583f41c76ce78888fb98faa1adc06c2c80b
James Smith <james@floppy.org.uk>
2014-02-13 15:47:29 +0000

    Test line break error display

--- spec/views/validation/validate.html.erb_spec.rb
+++ spec/views/validation/validate.html.erb_spec.rb
@@ -9,7 +9,8 @@ describe "validation/_message.html.erb" do
       :no_encoding => "The encoding of your CSV file is not being declared in the HTTP response.",
       :invalid_encoding => "Your CSV appears to be encoded in <code>iso-8859-1</code>, but invalid characters were found",
       :wrong_content_type => "Your CSV file is being delivered with an incorrect <code>Content-Type</code>",
-      :no_content_type => "Your CSV file is being delivered without a <code>Content-Type</code> header"
+      :no_content_type => "Your CSV file is being delivered without a <code>Content-Type</code> header",
+      :nonrfc_line_breaks => "Your CSV appears to use <code>LF</code> line-breaks"
     }.each do |k, v|
       
       message = Csvlint::ErrorMessage.new(:type => k)
@@ -18,6 +19,7 @@ describe "validation/_message.html.erb" do
       validator.stub(:content_type) { "text/plain" }
       validator.stub(:extension) { ".csv" }
       validator.stub(:headers) { {"content-type" => "text/plain"} }
+      validator.stub(:line_breaks) { "\n" }
         
       validator 
       render :partial => "validation/message", :locals => { :message => message, :validator => validator }
