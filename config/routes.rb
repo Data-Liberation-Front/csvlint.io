35bbb995b44cc6a6013fa7403ee3e17e16f458fb
James Smith <james@floppy.org.uk>
2014-02-14 11:30:13 +0000

    Add schema list view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Csvlint::Application.routes.draw do
   
+  get "schemas/index"
   root :to => 'validation#index'
   
   get 'validation/find_by_url', to: 'validation#find_by_url', as: 'find_by_url'
   get 'validation/list', to: 'validation#list', as: 'list'
   resources :validation 
+  resources :schemas
   
 end
