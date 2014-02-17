f003a33ac93aa03d0b9d1f83e9041a71f8dcceef
James Smith <james@floppy.org.uk>
2014-02-17 17:11:47 +0000

    Add simple page for displaying schema info

--- app/controllers/schemas_controller.rb
+++ app/controllers/schemas_controller.rb
@@ -4,5 +4,10 @@ class SchemasController < ApplicationController
     schemas = Schema.all
     @schemas = Kaminari.paginate_array(schemas).page(params[:page])
   end
+  
+  def show
+    db_schema = Schema.where(id: params[:id]).first
+    @schema = Csvlint::Schema.load_from_json_table(db_schema.url)
+  end
 
 end
