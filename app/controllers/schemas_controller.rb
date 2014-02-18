4bc44f4bf6790133754c8d94d37412bc1fd225dc
James Smith <james@floppy.org.uk>
2014-02-18 09:20:46 +0000

    303 Redirect from schemas?uri=xxx to schema page

--- app/controllers/schemas_controller.rb
+++ app/controllers/schemas_controller.rb
@@ -1,8 +1,12 @@
 class SchemasController < ApplicationController
 
   def index
-    schemas = Schema.all
-    @schemas = Kaminari.paginate_array(schemas).page(params[:page])
+    if params[:uri]
+      redirect_to Schema.where(url: params[:uri]).first, status: 303
+    else
+      schemas = Schema.all
+      @schemas = Kaminari.paginate_array(schemas).page(params[:page])
+    end
   end
   
   def show
--- spec/controllers/schemas_controller_spec.rb
+++ spec/controllers/schemas_controller_spec.rb
@@ -9,4 +9,14 @@ describe SchemasController do
     end
   end
 
+  describe "GET 'index' with uri arg" do
+    it "returns 303 redirect if schema is in DB" do
+      schema = FactoryGirl.create :schema, url: "http://data.com/schema.json"
+      get 'index', uri: "http://data.com/schema.json"
+      response.should be_redirect
+      response.code.should == "303"
+      response.location.should == "http://test.host/schemas/#{schema.id}"
+    end
+  end
+
 end
