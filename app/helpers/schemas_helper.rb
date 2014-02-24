918fa441ca7a9b72b6366b9d2b6b9bba3c47bae1
Leigh Dodds <leigh@ldodds.com>
2014-02-24 17:38:33 +0000

    Reworked schema page to break out constraints into separate columns

--- app/helpers/schemas_helper.rb
+++ app/helpers/schemas_helper.rb
@@ -1,2 +1,29 @@
 module SchemasHelper
+  
+  def date_type?(field)
+    ['http://www.w3.org/2001/XMLSchema#dateTime', 
+     'http://www.w3.org/2001/XMLSchema#date', 
+     'http://www.w3.org/2001/XMLSchema#time', 
+     'http://www.w3.org/2001/XMLSchema#gYear', 
+     'http://www.w3.org/2001/XMLSchema#gYearMonth'].include? field.constraints["type"]  
+  end
+  
+  def date_pattern(field)
+    return "" unless date_type?(field)
+    return field.constraints["datePattern"] if field.constraints["datePattern"]
+    return {
+      'http://www.w3.org/2001/XMLSchema#dateTime' => "%Y-%m-%dT%H:%M:%SZ",
+      'http://www.w3.org/2001/XMLSchema#date' => "%Y-%m-%d",    
+      'http://www.w3.org/2001/XMLSchema#time' => "%H:%M:%S", 
+      'http://www.w3.org/2001/XMLSchema#gYear' => "%Y", 
+      'http://www.w3.org/2001/XMLSchema#gYearMonth' => "%Y-%m"    
+    }[ field.constraints["datePattern"] ]
+  end
+  
+  def value_constraints(field)
+    field.constraints.reject do |k,v|
+      !["minimum", "minLength", "maximum", "maxLength", "pattern"].include?(k)
+    end  
+  end
+  
 end
--- spec/views/schemas/index.html.erb_spec.rb
+++ spec/views/schemas/index.html.erb_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
 
 describe "schemas/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
