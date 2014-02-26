c4e48f4a717f511d39234a09479441c909169723
pezholio <pezholio@gmail.com>
2014-02-26 13:47:14 +0000

    Fix cukes

--- features/step_definitions/datapackages_steps.rb
+++ features/step_definitions/datapackages_steps.rb
@@ -4,3 +4,15 @@ Then(/^the url "(.*?)" should be persisted in the database$/) do |url|
   filename = File.basename(URI.parse(url).path)
   Validation.first.filename.should == filename
 end
+
+Then(/^I should be redirected to my package page$/) do
+  current_path.should == package_path(Package.first)
+end
+
+Then(/^my datapackage should be persisited in the database$/) do
+  Package.first.url.should == @url
+end
+
+When(/^I click on the first report link$/) do
+  click_link("View report")
+end
