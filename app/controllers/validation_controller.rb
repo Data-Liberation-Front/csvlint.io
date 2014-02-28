b323c3aded2cf3693ef105fbe4fb1e4dc330e00a
pezholio <pezholio@gmail.com>
2014-02-28 16:00:04 +0000

    Refactor url stuff

--- app/controllers/validation_controller.rb
+++ app/controllers/validation_controller.rb
@@ -1,6 +1,7 @@
 require 'uri'
 
 class ValidationController < ApplicationController
+  before_filter :validate_urls, :only => :create
 
   def index
     if params[:uri]
@@ -10,13 +11,7 @@ class ValidationController < ApplicationController
     end
   end
 
-  def create
-    params[:urls].reject! { |url| url.blank? } unless params[:urls].blank?
-    
-    if validate_urls(params[:urls]) === false
-      redirect_to root_path and return
-    end    
-        
+  def create            
     load_schema
     package = check_for_package
     
@@ -63,21 +58,28 @@ class ValidationController < ApplicationController
   
   private
     
-    def validate_urls(urls)    
-      return true if params[:files].presence
-      return false if urls.blank?
-      
-      urls.each do |url|
-        return false if url.blank?
+    def validate_urls
+      remove_blanks!
+      valid = true
+      return if params[:files].presence
+      redirect_to root_path and return if params[:urls].blank?
+    
+      params[:urls].each do |url|
+        valid = false if url.blank?
         # Check it's valid
         begin
           url = URI.parse(url)
-          return false unless ['http', 'https'].include?(url.scheme)
+          valid = false unless ['http', 'https'].include?(url.scheme)
         rescue URI::InvalidURIError
-          return false
+          valid = false
         end
       end
-      return true
+      
+      redirect_to root_path and return if valid === false
+    end
+    
+    def remove_blanks!
+      params[:urls].reject! { |url| url.blank? } unless params[:urls].blank?
     end
     
     def load_schema
