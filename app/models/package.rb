1cddc443c9d23b0546a814de1df894ae61c55003
pezholio <pezholio@gmail.com>
2014-03-03 11:16:56 +0000

    Check for CKAN as well as Datapackage

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,47 +1,46 @@
 class Package
   include Mongoid::Document
   include Mongoid::Timestamps
-  
+
   field :url, type: String
   field :dataset, type: String
   field :type, type: String
-  
+
   has_many :validations
-  
+
   def self.parse_package(dataset)
     attributes = {
       :url => dataset.access_url,
       :dataset => Marshal.dump(dataset),
       :validations => [],
-      :type => "datapackage"
+      :type => dataset.publishing_format
     }
-    
+
     return attributes
   end
-  
+
   def self.create_package(sources, schema_url = nil, schema = nil)
     return nil if sources.count == 0
-        
+
     if sources.count == 1 && sources.first.class == String
       check_datapackage(sources.first)
     elsif sources.count > 1
       package = create({ type: set_type(sources) })
-      
+
       sources.each do |source|
         package.validations << Validation.create_validation(source, schema_url, schema)
       end
-          
+
       package.save
       package
     end
   end
-  
+
   def self.check_datapackage(url)
     dataset = DataKitten::Dataset.new(access_url: url)
-    return nil unless dataset.publishing_format == :datapackage
-    
+    return nil unless [:ckan, :datapackage].include? dataset.publishing_format
+
     package = create( parse_package(dataset) )
-    
     dataset.distributions.each do |distribution|
       if distribution.format.extension == :csv
         schema_desc = distribution.schema
@@ -54,10 +53,10 @@ class Package
     package.save
     package
   end
-  
+
   def self.set_type(sources)
     return "files" if sources.first.respond_to?(:tempfile)
     return "urls" if sources.first.class == String
   end
-  
+
 end
