7268d7db01ae0939480132a04ca0ce6c894e5e49
pezholio <pezholio@gmail.com>
2014-06-09 12:26:31 +0100

    Don't revalidate if revalidate is set to false

--- spec/models/validation_spec.rb
+++ spec/models/validation_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Validation do
-  
+
   it "should recheck validations after two hours" do
     mock_file("http://example.com/test.csv", 'csvs/valid.csv')
     validation = Validation.create_validation("http://example.com/test.csv")
@@ -12,5 +12,19 @@ describe Validation do
       validation.state.should == "invalid"
     end
   end
-  
+
+  it "should not requeue a validation for images if revalidate is set to false" do
+    mock_file("http://example.com/test.csv", 'csvs/valid.csv')
+    validation = Validation.create_validation("http://example.com/test.csv")
+    Validation.any_instance.should_not_receive(:delay)
+    Validation.fetch_validation(validation.id, "png", false)
+  end
+
+  it "should not revalidate for html if revalidate is set to false" do
+    mock_file("http://example.com/test.csv", 'csvs/valid.csv')
+    validation = Validation.create_validation("http://example.com/test.csv")
+    Validation.any_instance.should_not_receive(:check_validation)
+    Validation.fetch_validation(validation.id, "html", false)
+  end
+
 end
