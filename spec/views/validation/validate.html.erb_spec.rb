40c42ad01c1338330061e47d023d7dfd0f3bc1ca
pezholio <pezholio@gmail.com>
2014-06-09 12:27:18 +0100

    Add revalidate param to images on validation list

--- spec/views/validation/validate.html.erb_spec.rb
+++ spec/views/validation/validate.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "validation/_message.html.erb" do
 
   it "displays errors and warnings correctly" do
-    
+
     {
       :encoding => "Your CSV appears to be encoded in <code>iso-8859-1</code>. We recommend you use <code>UTF-8</code>.",
       :no_encoding => "The encoding of your CSV file is not being declared in the HTTP response.",
@@ -12,7 +12,7 @@ describe "validation/_message.html.erb" do
       :no_content_type => "Your CSV file is being delivered without a <code>Content-Type</code> header",
       :nonrfc_line_breaks => "Your CSV appears to use <code>LF</code> line-breaks"
     }.each do |k, v|
-      
+
       message = Csvlint::ErrorMessage.new(:type => k)
       validator = double("validator")
       validator.stub(:encoding) { "iso-8859-1" }
@@ -21,13 +21,26 @@ describe "validation/_message.html.erb" do
       validator.stub(:headers) { {"content-type" => "text/plain"} }
       validator.stub(:line_breaks) { "\n" }
       validator.stub(:schema) { nil }
-        
-      validator 
+
+      validator
       render :partial => "validation/message", :locals => { :message => message, :validator => validator }
-      
+
       rendered.should include v
     end
-    
+
+  end
+
+end
+
+describe "validation/_validation.html.erb" do
+
+  it "Should not revalidate" do
+    mock_file("http://example.com/test.csv", 'csvs/valid.csv')
+    Validation.create_validation('http://example.com/test.csv')
+    validation = Validation.first
+
+    validation.should_not_receive(:delay)
+    render :partial => "validation/validation", :locals => { :validation => validation }
   end
 
 end
