5d7f7ceb8535e36a861d12c7c9e0158c15fd7e8f
pezholio <pezholio@gmail.com>
2015-08-27 13:42:30 +0100

    Avoid name collisions when unzipping
    I've added comments to be more precise about what's going on. This whole thing could use a refactor though

--- lib/zipfile.rb
+++ lib/zipfile.rb
@@ -27,8 +27,10 @@ class Zipfile
 
   def self.unzip(source, files, type)
     if type == :urls
-      FileUtils.mkdir_p(File.join('/', 'tmp', 'csvs', DateTime.now.to_s))
-      file = File.new(File.join('/', 'tmp', 'csvs', DateTime.now.to_s, source.split("/").last), "w+")
+      # Create a uniquely named folder to drop the downloaded file into
+      folder = FileUtils.mkdir_p(File.join('/', 'tmp', 'csvs', source.split("/").last + Time.now.to_i.to_s))
+      # Create the file in the new folder and write the contents of the URL into it
+      file = File.new(File.join(folder, source.split("/").last), "w+")
       file.binmode
       file.write open(source).read
       file.rewind
@@ -37,10 +39,16 @@ class Zipfile
     end
 
     Zip::File.open(file) do |zipfile|
+      # Create a uniquely named folder to unzip into (avoids name collisions)
+      folder_name = (File.basename(file) + Time.now.to_i.to_s).parameterize
+      folder = File.join(File.expand_path("..", file), folder_name)
+      FileUtils.mkdir_p(folder)
       zipfile.each do |entry|
         next if entry.name =~ /__MACOSX/ or entry.name =~ /\.DS_Store/
-        destination = File.join(File.expand_path("..", file), entry.name)
+        # Unzip into the created folder
+        destination = File.join(folder, entry.name)
         entry.extract(destination)
+        # Add the file location to the files array - We drop the /tmp part of the folder name, as the controller assumes this already
         files << destination.split("/").drop(2).join("/")
       end
     end
