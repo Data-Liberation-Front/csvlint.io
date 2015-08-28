2d698708bbc1f9c5d4a450e63ff6ba97d0758451
pezholio <pezholio@gmail.com>
2015-08-28 13:25:54 +0100

    Always store filename

--- lib/stored_csv.rb
+++ lib/stored_csv.rb
@@ -2,9 +2,9 @@ require 'mongoid/grid_fs'
 
 class StoredCSV
 
-  def self.save(file, filename = nil)
+  def self.save(file, filename)
     stored_csv = Mongoid::GridFs.put(file)
-    stored_csv.metadata = { filename: filename } if filename
+    stored_csv.metadata = { filename: filename }
     stored_csv.save
 
     file.close
