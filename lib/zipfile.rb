889429814b736a157fc695c2a2232ec0c8749de6
pezholio <pezholio@gmail.com>
2015-08-28 15:31:13 +0100

    Trim down unzip method

--- lib/zipfile.rb
+++ lib/zipfile.rb
@@ -2,30 +2,7 @@ require 'zip'
 
 class Zipfile
 
-  def self.check!(p)
-    if p[:urls].presence && p[:urls].count > 0
-      type = :urls
-    elsif p[:files].presence
-      type = :files
-    end
-
-    files = []
-    p[type].each do |source|
-      if Zipfile.is_zipfile?(source)
-        Zipfile.unzip(source, files, type)
-      else
-        files << source
-      end
-    end
-    p[type] = files
-    p
-  end
-
-  def self.is_zipfile?(source)
-    File.extname(source) == ".zip"
-  end
-
-  def self.unzip(source, files, type)
+  def self.unzip(source, type)
     if type == :urls
       file = Tempfile.new(source.split("/").last)
       file.binmode
@@ -35,13 +12,20 @@ class Zipfile
       file = File.open(source)
     end
 
+    files = []
+
     Zip::File.open(file) do |zipfile|
       zipfile.each do |entry|
         next if entry.name =~ /__MACOSX/ or entry.name =~ /\.DS_Store/
-        destination = Tempfile.new(entry.name)
-        files << destination.path
+        file = entry.get_input_stream
+        csv = StoredCSV.save(file, entry.name)
+        files << {
+          :csv_id => csv.id,
+          :filename => csv.metadata[:filename]
+        }
       end
     end
+    files
   end
 
 end
