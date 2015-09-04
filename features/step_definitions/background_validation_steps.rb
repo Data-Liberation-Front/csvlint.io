c10465e4a140b66ba4681c021b261d6633c72bc5
pezholio <pezholio@gmail.com>
2015-09-04 15:16:28 +0100

    Fix background validation cukes

--- features/step_definitions/background_validation_steps.rb
+++ features/step_definitions/background_validation_steps.rb
@@ -1,5 +1,7 @@
+require 'process_package'
+
 Then(/^my CSV should be placed in a background job$/) do
-  Package.any_instance.should_receive(:delay).and_call_original
+  ProcessPackage.any_instance.should_receive(:delay).and_call_original
 end
 
 When(/^I wait for the ajax request to finish$/) do
