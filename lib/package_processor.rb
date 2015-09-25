f33cd3f9ce145ec2c9c19a57714f6979479bacb4
quadrophobiac <stephen.j.fortune@gmail.com>
2015-09-25 17:08:54 +0100

    code now works for schemas but this wretched line `<input type="hidden" name="file_ids[]" />` has to be included otherwise a solid chunk of cucumber fails, therefore a new way of ensuring that chunk_files methods isn't invoked is required

--- lib/package_processor.rb
+++ lib/package_processor.rb
@@ -16,6 +16,7 @@ class PackageProcessor
     # TODO when the file_helper is used without JS it is sending params with both  :file_ids and :files, triggering join_chunks AND open_files
     read_files unless @params[:files_data].blank?
     join_chunks unless @params[:file_ids].blank?
+    # join_chunks if @params[:file_ids].any?
     # TODO - the above is always triggered, need to know why that is
     open_files unless @params[:files].blank?
     unzip_urls unless @params[:urls].blank?
@@ -28,10 +29,12 @@ class PackageProcessor
   end
 
   def create_package
-    if @params[:schema].nil?
+    if (@params[:schema_data].nil? && @params[:schema_file].nil? && @params[:schema_url].nil?)
       package.create_package(@files || @params[:urls])
     else
       schema = SchemaProcessor.new(url: @params[:schema_url], file: @params[:schema_file], data: @params[:schema_data])
+      # TODO - fallback schema upload does not have schema or schema_data attributes
+      # byebug
       package.create_package(@files || @params[:urls], schema.url, schema.schema)
       # TODO the problem might be happening here - if @files has too many variables
       # then sources.each do |source| block creates extra validations
