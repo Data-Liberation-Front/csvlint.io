2e5e556594c22783952f011c7700f7a9ba0c079f
James Smith <james@floppy.org.uk>
2017-07-08 12:01:31 +0100

    Update airbrake config

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,6 @@
-if ENV['CSVLINT_AIRBRAKE_KEY']
+if ENV['AIRBRAKE_API_KEY']
   Airbrake.configure do |config|
-    config.api_key = ENV['CSVLINT_AIRBRAKE_KEY']
+    config.project_id = ENV['AIRBRAKE_PROJECT_ID']
+    config.project_key = ENV['AIRBRAKE_API_KEY']
   end
 end
