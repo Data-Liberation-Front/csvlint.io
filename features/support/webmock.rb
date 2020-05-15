a88e24fe4111b8696868a7bf751b1239841679a0
James Smith <james@floppy.org.uk>
2020-05-16 00:05:12 +0100

    better csvw stubbing in cukes

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -1,5 +1,1 @@
 require 'webmock/cucumber'
-WebMock.disable_net_connect!(
-          :allow => [/static.(dev|theodi.org)/, /datapackage\.json/, /package_search/],
-          :allow_localhost => true
-          )
